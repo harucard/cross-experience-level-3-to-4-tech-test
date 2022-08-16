ef59c6e7868b741facb36a3cd7f4fe3e9a3b735e
Roquelson <roquelsonferreira@assistevida.com>
2022-08-15 22:45:45 -0300

    Estilizando componente de plano pessoal

--- components/plans/Personal.jsx
+++ components/plans/Personal.jsx
@@ -1,30 +1,32 @@
 
 const Personal = ({plan}) => (
    <>
-      <div className="uk-card uk-card-default uk-card-small uk-card-body uk-position-relative    uk-child-width-1-1@s"  data-uk-grid data-uk-height-match="target: > .uk-flex-row > div">
-      
-        <div className="uk-text-center uk-padding-remove-left">
-            <h4 className="uk-padding-small uk-text-bold">{plan.offerInfo.name}</h4>
-        </div>  
-        
-            { plan.offerInfo.benefits.map((item)=>(
-                <div key={`container_${item}`} className="uk-flex uk-flex-row uk-margin-remove-top  uk-margin-remove-left">
-                    <div>
-                        <p data-uk-icon="icon: check; ratio:1" className="uk-text-success"></p>
+      <div className="uk-card uk-card-default uk-position-relative">
+            <div className="uk-text-center uk-margin-top">
+                <h3 className="uk-card-title uk-margin-remove ">{plan.offerInfo.name}</h3>
+            </div>
+            <div className="uk-card-body plan-card-body">
+            { plan.offerInfo.benefits.map((item)=>(  
+                <div key={`container_${item}`} className="uk-flex ">
+                    <div className=" uk-margin-right">
+                        <i class="uk-icon-button uk-text-success" data-uk-icon="icon: check; ratio:1"></i>
                     </div>
                     <div>
-                        <p className="uk-text-default uk-text-left"  key={`text_${item}`}>{item}</p>
+                    <p className="uk-text-small uk-text-secondary uk-text-left" key={`text_${item}`}>{item}</p>
                     </div>
-                </div>                
+                </div>            
+                           
+                    
             ))
+            
             }
-        
-        <div className="uk-text-center  uk-margin-remove-bottom uk-margin-remove-top">
+            </div>
+            <div className=" uk-card-footer   uk-text-center uk-margin-top">
             <p><span className="uk-margin-small-left uk-text-emphasis">{plan.paymentInfo.currency}</span><strong>{plan.paymentInfo.price}</strong>/mês</p>
             <a className="uk-button uk-button-primary uk-width-1-1 uk-margin-remove-bottom" href={`/checkout?name=${plan.offerInfo.name}&price=${plan.paymentInfo.price}`}>ASSINAR AGORA</a>
             <p className="uk-text-muted uk-margin-remove-top">Cancele quando quiser</p>
-        </div>
-       
+            </div>
+      
       </div>
    </>
   );
